91aaf82eb5fb885e4d286a4f426b4d6725352107
israa-mazloum <128637659+israa-mazloum@users.noreply.github.com>
2024-03-26 13:05:01 +0300

    html-css task 1 yapıldı

--- 04-javascript-kategorisi/js/011-prompt-ile-kullanicidan-bilgi-almak.js
+++
@@ -1,8 +0,0 @@
-// ********** prompt ile Kullanicidan Bilgi Almak **********
-// https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt
-
-let fullName = prompt("Lutfen Adinizi Giriniz: ")
-
-let greeting = document.querySelector("#greeting")
-
-greeting.innerHTML = `${greeting.innerHTML} <small style="color:red">${fullName}</small>`
--- 04-javascript-kategorisi/js/027-form-submit-yonetimi.js
+++
@@ -1,9 +0,0 @@
-// FORM SUBMIT:
-
-let formDOM = document.querySelector("#userForm")
-formDOM.addEventListener('submit', formSubmit)
-
-function formSubmit(event) {
-    event.preventDefault() // default islemi engelledik...
-    console.log("islem gerceklesti")
-}
